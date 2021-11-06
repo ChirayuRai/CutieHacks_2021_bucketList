d2b500f6199b9ebf428b810eae8f5788e6d6c2e1
Chirayu Rai <chirayurai@gmail.com>
2021-11-06 14:09:23 -0700

    seperating workload

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,32 @@ module.exports = {
   theme: {
     container: {
       center: true,
-      padding: '2rem'
     },
+    colors: {
+      yellow: {
+        DEFAULT: "#e9ba03",
+        light: "#fbf2cf",
+      },
+      brown: {
+        DEFAULT: "#A07241",
+      },
+      blue: {
+        light: "b9d9f0",
+        DEFAULT: "#83c4ec",
+        dark: "#38a0d2",
+      },
+      white: {
+        DEFAULT: "#FFFFFF",
+      },
+      black: {
+        DEFAULT: "#000000",
+      },
+      extend: {
+        fontFamily: {
+          body: ["Roboto"]
+        }
+      }
+    }
   },
   variants: {
     extend: {},
